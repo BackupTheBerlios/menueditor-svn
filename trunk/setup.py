95b09e4598e97aadd6c0f3c8727ffa847b9667cf
amaranth <amaranth@ed01b071-e5f2-0310-aa68-a0d3e5870c63>
2005-04-26 06:37:57 +0000

    initial import

---
+++ trunk/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os, sys
+
+libdir = 'lib/smeg'
+sys.path = [os.path.join(os.curdir, libdir)] + sys.path
+
+from distutils.core import setup
+import glob
+
+setup(name = "Smeg",
+      version = '0.5',
+      description = "Simple Menu Editor for GNOME",
+      author = "Travis Watkins",
+      url = "http://www.realistanew.com/projects/smeg/",
+      scripts = ['bin/smeg'],
+      data_files = [(libdir, glob.glob(os.path.join(libdir, '*.py'))),
+                    ('share/applications',['smeg.desktop']),
+                    ('share/pixmaps', ['smeg.png']),
+                   ],
+      license = 'GNU GPL',
+      platforms = 'posix',
+      )
